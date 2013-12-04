99547d42e132e465bbe244ebb0810603235d54ad
amoghavs <amoghavs.gmail.com>
2013-12-03 18:13:12 -0800

    Extracting info from spatial files

--- ConfigGenerator.py
+++ ConfigGenerator.py
@@ -11,23 +11,21 @@ def main():
 	Min={}
 	Max['Vars']=1
 	Min['Vars']=1
-	Max['Dims']=5
+	Max['Dims']=2
 	Min['Dims']=1
-	Max['Stride']=0 # ie., 2^4
-	Min['Stride']=0 # ie., 2^0=1
+	Max['Stride']=2 # ie., 2^4
+	Min['Stride']=2 # ie., 2^0=1
 	Alloc=['d']	
 	Init='index0*10+index0'
 	DS='i'
 	SpatWindow=[8,16,32];
-	MbyteSize=28 # 32Mbyte= 2^20[1M] * 2^5 [32] * 2^3[byte]
+	MbyteSize=12 # 32Mbyte= 2^20[1M] * 2^5 [32] * 2^3[byte]
 	MaxSize=2**MbyteSize
 	Dim0Size=2**(MbyteSize-8)
 	HigherDimSize= MaxSize/	Dim0Size
 	for NumVars in range(Min['Vars'],Max['Vars']+1):
 		for NumDims in range(Min['Dims'],Max['Dims']+1):
 
-			
-			#print "\n\t SizeString: "+str(SizeString)+" and SizeName: "+str(SizeName)
 			for BaseOfStride in range(Min['Stride'],Max['Stride']+1):
 				Stride=2**BaseOfStride
 				SizeString=''
@@ -72,37 +70,52 @@ def main():
 					f.write("\n#init "+str(Init))
 					f.write("\n#datastructure "+str(DS))
 					f.close()
-					#CMDmvConfigFile='mv '+str(ConfigFile)+' '+str(Config) 
-					#commands.getoutput(CMDmvConfigFile)					
-					#CMDcpStrideBenchmarks='cp StrideBenchmarks.py '+str(Config)
-					#commands.getoutput(CMDcpStrideBenchmarks)
+
 					CMDrunStrideBenchmarks='python StrideBenchmarks.py -c '+str(ConfigFile)
-					#print "\n\t Run: "+str(CMDrunStrideBenchmarks)
 					commands.getoutput(CMDrunStrideBenchmarks)
 					SRCCode='StrideBenchmarks_'+str(UniqueID)+'.c'
 					EXE='StrideBenchmarks_'+str(UniqueID)
-					#print "\n\t SRC: "+str(SRCCode)
-					#CMDmvSRCCode='mv '+str(SRCCode)+' '+str(Config)
-					#commands.getoutput(CMDmvSRCCode)
 					CMDCompileSRC='gcc -g '+str(SRCCode)+' -o '+str(EXE)
 					commands.getoutput(CMDCompileSRC)
 					CMDPebilCompile='pebil --typ sim --inp SimInp.log --app '+str(EXE)
 					commands.getoutput(CMDPebilCompile)
 					
+					
+					SWStats=open(str('SWStats_'+str(Config)+'.txt'),'w')
 					for CurrSW in SpatWindow:
+						SWStats.write("\n---------------------")
+						SWStats.write("\n\t Spatial-window size: "+str(CurrSW)+"\n\n")
 						CMDExportSW='export METASIM_SPATIAL_WINDOW='+str(CurrSW)
-						#commands.getoutput(CMDExportSW)
-						#
 						SimInst=str(EXE)+'.siminst'
 						CMDRunSiminst='./'+str(SimInst)
 						CMDExportSW+=' |'+CMDRunSiminst
 						commands.getoutput(CMDExportSW)
-						#print "\n\t "+str(CMDExportSW)
-						CMDRenameSpatial='mv *.spatial'+' SW_'+str(CurrSW)+'_'+str(Config)
+						SWFile='SW_'+str(CurrSW)+'_'+str(Config)+'.log'
+						CMDRenameSpatial='mv *.spatial '+str(SWFile)
 						commands.getoutput(CMDRenameSpatial)
-					
+						CMDgrep='grep Total '+str(SWFile)
+						GrepOutput=commands.getoutput(CMDgrep)
+
+						Access=re.match(r'\s*.*\:\s*(\d+)+$',GrepOutput)
+						if Access:
+							TotalAccess=int(Access.group(1))
+							#print "\n\t Total Accesses as found out: "+str(TotalAccess)+" and grep output: "+str(GrepOutput)							
+						
+						f=open(SWFile,'r')
+						SWFileContents=f.readlines()
+						f.close()
+						SWStats.write( "\n\t Bin: \t\t Range: \t\t Count: \t\t % ")						
+						for CurrLine in SWFileContents:
+							Data=re.match(r'\s*.*Bin\:\s*(\d+)+.*Range\:\s*(\d+)+.*Count\:\s*(\d+)+$',CurrLine)	
+							if Data:
+								SWStats.write( "\n\t "+str(Data.group(1))+"\t\t "+str(Data.group(2))+"\t\t "+str(Data.group(3))+"\t\t "+str( 100* float(Data.group(3)) / TotalAccess )  )
+								#SWStats.write( "\n\t Bin: "+str(Data.group(1))+"\t Range: "+str(Data.group(2))+"\t Count: "+str(Data.group(3))+"\t % "+str( 100* float(Data.group(3)) / TotalAccess )  )
+								#print "\n\t Bin: "+str(Data.group(1))+" Range: "+str(Data.group(2))+" Count: "+str(Data.group(3))+" % "+str( 100* float(Data.group(3)) / TotalAccess ) 
+						
+						SWStats.write("\n\n")		
+						
+					SWStats.close()
 					CMDMvAll='mv *.c SW* *siminst* '+str(ConfigFile)+' '+str(EXE)+' '+str(Config)
-					print "\n\t Mv command: "+str(CMDMvAll)
 					commands.getoutput(CMDMvAll)
 					CMDRmMetaFiles='rm -f *Instructions* LRU*'
 					commands.getoutput(CMDRmMetaFiles)
